0f418665291575522605b3f339aebd75b31d87d1
unknown-cat <twilightcat73@yahoo.com>
2022-05-17 17:15:05 +0300

    add navigation bar and linking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './components/routes/home/home.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='hey' element={<h1>hey</h1>} />
+      </Route>
     </Routes>
   );
 };
