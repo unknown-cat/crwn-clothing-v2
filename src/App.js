ebce01f57daeee8d13f0655a6d7d2002b9268b59
unknown-cat <twilightcat73@yahoo.com>
2022-05-25 14:44:29 +0300

    feat: render shop component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='hey' element={<h1>hey</h1>} />
+        <Route path='shop' element={<Shop/>} />
       </Route>
     </Routes>
   );
