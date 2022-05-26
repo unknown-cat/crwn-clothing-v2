0ddf3eb93e7808705428bf0cb093c43ab156e3e8
unknown-cat <twilightcat73@yahoo.com>
2022-05-26 14:11:19 +0300

    refactor: rename provider
    due to lack of explicity

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -18,13 +18,14 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
 
@@ -50,6 +51,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
       console.log('error creating user ', error.message);
     }
   }
-  console.log(userAuth)
+  console.log(userAuth);
   return userDocRef;
 };
