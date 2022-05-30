4e3e4e790f64001e1a52ef0f351ca757c55f3a2c
unknown-cat <twilightcat73@yahoo.com>
2022-05-30 18:26:19 +0300

    feat: add button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -49,8 +54,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <section>
-      <h2>Sign up with your email and password</h2>
+    <section className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label='Display Name'
@@ -93,7 +99,7 @@ const SignUpForm = () => {
           required
         />
 
-        <button type='section'>Sign Up</button>
+        <Button type='section'>Sign Up</Button>
       </form>
     </section>
   );
